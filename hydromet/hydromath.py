e3803b7e55baa336417f2b47073826f12f3abed7
Andrew MacDonald <andrew@maccas.net>
2015-10-26 22:00:44 +1100

    Weird hacking to find libhydromath.so on Linux
    This is needed since Linux ctypes.util.find_library doesn't return full
    path to the library. Whereas on Mac OS X it does.
    
    Surely there is a better way to go about this, but I haven't been able
    to identify it.

--- hydromet/hydromath.py
+++ hydromet/hydromath.py
@@ -3,10 +3,24 @@ import os
 from ctypes.util import find_library
 from cffi import FFI
 
-prefix = find_library('hydromath').split('/lib/')[0]
+search_dirs = [
+    '/usr/local/include',
+    '/usr/include',
+]
 
-__ffi = FFI()
-with open(os.path.join(prefix, 'include', 'hydromath.h')) as header:
-    __ffi.cdef(header.read())
+h_filename = None
 
-__lib = __ffi.dlopen('hydromath')
+for directory in search_dirs:
+    tmp_name = os.path.join(directory, 'hydromath.h')
+    if os.path.isfile(tmp_name) or os.path.islink(tmp_name):
+        h_filename = tmp_name
+        break
+else:
+    print("hydromath.h not found, is it installed?")
+
+if h_filename is not None:
+    __ffi = FFI()
+    with open(h_filename) as header:
+        __ffi.cdef(header.read())
+
+    __lib = __ffi.dlopen('/usr/local/lib/libhydromath.so')
