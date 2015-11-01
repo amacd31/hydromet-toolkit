70487628cea0a08ec25ea8d7e21677ecc042f8e0
Andrew MacDonald <andrew@maccas.net>
2015-11-01 22:04:29 +1100

    Update diff evolution run script
    Updated to skip sites missing data starting loop at point where we last
    left off.

--- de_opt_gr4j.py
+++ de_opt_gr4j.py
@@ -8,20 +8,26 @@ from hydromet.io import read_predictors
 from hydromet.system import get_model_config
 
 from phildb.database import PhilDB
+from phildb.exceptions import MissingDataError
 
 db = PhilDB('hm_tsdb')
-config = get_model_config('project', 'gr4j')
+config = get_model_config('model_config', 'gr4j')
 
-for station_id in ['410730']:
-    area = np.loadtxt('data/catchment_grids/{0}.area'.format(station_id))
-    q = db.read(station_id, 'D', source='BOM_HRS', measurand='Q') / area
-    predictors = read_predictors(db, config, station_id)
+for station_id in db.ts_list(source='BOM_HRS', measurand='Q')[62:]:
+    print("Calibrating {0}".format(station_id))
+
+    try:
+        area = np.loadtxt('data/catchment_grids/{0}.area'.format(station_id))
+        q = db.read(station_id, 'D', source='BOM_HRS', measurand='Q') / area
+        predictors = read_predictors(db, config, station_id)
+    except MissingDataError:
+        continue
 
     model = GR4J()
 
     bounds = [(10,1500), (-5, 5), (10, 900), (0,5)]
     results = []
-    res_nse = calibrate('project', model, bounds, predictors, q)
+    res_nse = calibrate('model_config', model, bounds, predictors, q)
     results.append({'efficiency': 1 - res_nse.fun,
             'efficiency_type': 'nse',
             'X1': res_nse.x[0],
@@ -33,4 +39,4 @@ for station_id in ['410730']:
 
     print(results)
 
-    json.dump(results, open('de_opt_{0}_params.json'.format(station_id),'w'))
+    json.dump(results, open('results/de_opt_{0}_params.json'.format(station_id),'w'))
