21bf841795d93c763d82aae6b1659b21a530ce17
Andrew MacDonald <andrew@maccas.net>
2016-06-13 11:53:16 +1000

    Add reading and loading of streamflow from WDO
    WDO = Water Data Online; http://www.bom.gov.au/waterdata/

--- hydromet/loader.py
+++ hydromet/loader.py
@@ -1,5 +1,6 @@
 import zipfile
 import bom_data_parser as bdp
+from kiwis_pie import KIWIS
 from phildb.exceptions import DuplicateError
 
 def load_cdo(db, files):
@@ -50,3 +51,60 @@ def load_cdo(db, files):
 
         db.write(station_id, 'D', data[data_column_name], source='BOM_CDO', measurand=measurand)
 
+def read_streamflow(station_id, from_date = '1900-01-01', to_date = '2100-01-01'):
+    k = KIWIS('http://www.bom.gov.au/waterdata/services')
+
+    ts_id = k.get_timeseries_list(
+        station_no = '410730',
+        ts_name = 'PR01QaQc.Merged.DailyMean.09HR',
+        return_fields = [
+            'station_no',
+            'ts_id',
+            'ts_name',
+            'parametertype_name'
+        ]
+    )['ts_id']
+
+    if len(ts_id) != 1:
+        raise ValueError("Unable to get unique series from WISKI for '{0}'".format(station_id))
+
+    sf = k.get_timeseries_values(
+        ts_id = ts_id,
+        to = to_date,
+        return_fields = ['Timestamp', 'Value'],
+        **{'from': from_date}
+    )
+
+    if len(sf) == 0:
+        raise ValueError("No streamflow could be read.")
+
+    sf.index = sf.index.normalize()
+
+    return sf
+
+
+def load_streamflow(db, station_id):
+
+    sf = read_streamflow(station_id)
+
+    try:
+        db.add_timeseries(station_id)
+    except DuplicateError:
+        pass
+
+    try:
+        db.add_source('BOM_WISKI', 'Bureau of Meterology Water Data Online')
+    except DuplicateError:
+        pass
+
+    try:
+        db.add_measurand('Q', 'STREAMFLOW', 'Streamflow')
+    except DuplicateError:
+        pass
+
+    try:
+        db.add_timeseries_instance(station_id, 'D', '', source='BOM_WISKI', measurand='Q')
+    except DuplicateError:
+        pass
+
+    db.write(station_id, 'D', sf, source='BOM_WISKI', measurand='Q')
